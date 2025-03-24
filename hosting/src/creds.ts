94081cd59a9d36b42b994229d459bb17cacd0078
jckend <142367196+jckend@users.noreply.github.com>
2025-03-24 14:11:10 -0400

    Update creds.ts

--- hosting/src/creds.ts
+++ hosting/src/creds.ts
@@ -2,14 +2,13 @@ import type { FirebaseOptions } from 'firebase/app'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseCred: FirebaseOptions = {
-  apiKey: "AIzaSyBH40Rzp_I2kZPWtnLZo3FAXGZtm5D9yeo",
-  authDomain: "scalar-3ac8c.firebaseapp.com",
-  databaseURL: "https://scalar-3ac8c-default-rtdb.firebaseio.com",
-  projectId: "scalar-3ac8c",
-  storageBucket: "scalar-3ac8c.firebasestorage.app",
-  messagingSenderId: "529117945658",
-  appId: "1:529117945658:web:f103acf76b0d90804e7a03",
-  measurementId: "G-D77WVWF8BE"
+  apiKey: "AIzaSyDCg-bvYzRNHHIF8LJazoU1vxNkabE7ojU",
+  authDomain: "scalar2-1f01f.firebaseapp.com",
+  projectId: "scalar2-1f01f",
+  storageBucket: "scalar2-1f01f.firebasestorage.app",
+  messagingSenderId: "315511341144",
+  appId: "1:315511341144:web:91222fe5e80cc77a67ca2d",
+  measurementId: "G-CC9FECHDR4"
 }
 
 export const prolificCompletionCode: string = 'C2TJMYKC'
