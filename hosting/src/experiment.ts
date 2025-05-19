a333b473e2339914d33f9a1b6eedaeb5550b7fb4
jckend <142367196+jckend@users.noreply.github.com>
2025-05-19 00:09:41 -0400

    Update experiment.ts

--- hosting/src/experiment.ts
+++ hosting/src/experiment.ts
@@ -188,7 +188,7 @@ export async function runExperiment(updateDebugPanel: () => void): Promise<void>
   const test1 = {
     type: jsPsychHtmlSliderResponse,
     stimulus: function(){
-                var html = jsPsych.timelineVariable('stimulus1')} + jsPsych.timelineVariable('prompt1')};
+                var html = jsPsych.timelineVariable('stimulus1')} + jsPsych.timelineVariable('prompt1');
                 return html;
     },          
     labels: ['0%', '50%','100%'],
